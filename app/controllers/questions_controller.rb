78174d0a0bd1f67a0f8d890d7fe2837d400e7348
Vidushani Nishshanka <dvnishshanka@gmail.com>
2022-10-04 20:23:37 +0200

    Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = case @question
+              when 'I am going to work'
+                'Great!'
+              when /\? *\z/
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
